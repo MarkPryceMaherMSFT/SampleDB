e1b94f0c8904ff34e28d7ca3e2d6286e1f64dd1b
markprycemaher <markpm@hotmail.co.uk>
2018-02-01 15:29:27 +0000

    Add project files.

---
+++ MyDatabase/dbo/Tables/sysMessages.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[sysMessages] (
+    [message_id]      INT             NOT NULL,
+    [language_id]     SMALLINT        NOT NULL,
+    [severity]        TINYINT         NULL,
+    [is_event_logged] BIT             NOT NULL,
+    [text]            NVARCHAR (2048) NOT NULL
+);
+
